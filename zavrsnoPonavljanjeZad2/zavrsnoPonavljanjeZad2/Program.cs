e630cf1631cc56f8602e7ca82c002fcec0a40501
Neven <58472164+neven14@users.noreply.github.com>
2021-05-31 09:34:34 +0200

    Add files via upload

---
+++ zavrsnoPonavljanjeZad2/zavrsnoPonavljanjeZad2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoPonavljanjeZad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ukSekundi, min, sec;
+            string time;
+            Console.Write("Unesi vrijeme u sekundama: ");
+            ukSekundi = Convert.ToInt32(Console.ReadLine());
+
+            sec = ukSekundi % 60;
+            min = ukSekundi / 60;
+            time = min + ":" + sec;
+            Console.WriteLine(time);
+
+            Console.ReadKey();
+        }
+    }
+}
